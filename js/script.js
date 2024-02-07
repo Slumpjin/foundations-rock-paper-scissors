eed74c76b1d929e6e4aa5a9f208c6b014d13f3f0
Slumpjin <tap4302@gmail.com>
2024-02-06 22:44:10 -0600

    Write out js methods with pseudocode

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+function getComputerChoice() {
+    // generate a random number between 1-3 inclusive
+    
+    // switch through cases and return a string
+
+}
+
+function playRound(playerSelection, computerSelection) {
+    // make player selection case insensitive
+
+    // check for a tie
+
+    // check for cases where computer beats player
+
+    // else return a win for the player
+}
+
+function playGame() {
+    // create counter variables for the current game iteration and scores
+
+    // for each round, prompt the user and display round results
+
+    // show the final score
+}
