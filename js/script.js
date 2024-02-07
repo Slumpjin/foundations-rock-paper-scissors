7bf13a53e1669e6b9e0208c1be6ce2b4c18832bd
Slumpjin <tap4302@gmail.com>
2024-02-06 22:57:08 -0600

    Implement the getComputerChoice method

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 
 function getComputerChoice() {
     // generate a random number between 1-3 inclusive
-    
+    const randomHandSign = Math.ceil(Math.random() * 3);
     // switch through cases and return a string
-
+    switch (randomHandSign) {
+        case 1:
+            return 'rock';
+        case 2:
+            return 'paper';
+        case 3:
+            return 'scissors';
+    }
 }
 
 function playRound(playerSelection, computerSelection) {
